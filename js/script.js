86186eef7862ac68d90f53936df486cb8cba4f8b
mr-jamper <88425821+mr-jamper@users.noreply.github.com>
2021-10-04 15:08:18 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$('.sl').slick({
+   
+    autoplaySpeed:2000,
+    speed:2000,
+    slidesToShow: 4,
+    slidesToScroll:1 ,
+    asNavFor: '.sl2',
+    // arrows:true,
+    centerMode:true,
+    focusOnSelect: true,
+    infinite:false,
+     // dots:true
+});
+  
+  $('.sl2').slick({
+    // autoplay:true,
+    autoplaySpeed:2000,
+    speed:2000,
+    slidesToShow: 1,
+    // centerMode:true,
+    arrows:true,
+
+    // centerPadding:'40px',
+    // lazyLoad: 'ondemand',
+    // rows:2,
+    // slidesPerRow:2,
+    // cssEase:'ease-in',
+    // centerMode:true,
+    // dots:true,
+    // dotsClass: 'my-dots',
+    asNavFor: '.sl',
+    focusOnSelect: true,
+     infinite:false,
+    //  prevArrow: "<img src='https://svgshare.com/i/aoj.svg' class='prev' alt='1'>",
+    // nextArrow: "<img src='https://svgshare.com/i/amG.svg' class='next' alt='2'>",
+    
+  });
+
+  
